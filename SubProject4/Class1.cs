d50571e3adf9ff719d7dc7fcf58c16f98f693622
EFIF\aabd <aabd@aabc.dk>
2021-12-08 10:21:18 +0100

    Testing

--- SubProject4/Class1.cs
+++ SubProject4/Class1.cs
@@ -3,6 +3,11 @@
 
     public class Class1
     {
+        private string? ThisIsMyProp { get; set; }
+        private string? NotMyProp { get; set; }
 
+        private string? ThisIsYourProp { get; set; }
+        private string? NotYourProp { get; set; }
+        private string? NotYourProp { get; set; }
     }
 }
